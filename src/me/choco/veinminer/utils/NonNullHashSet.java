179e8a8df76ea86f835a72a96986b693ac3fc5ad
Parker Hawke <hawkeboyz2@hotmail.com>
2018-06-29 16:49:30 -0400

    Non-null HashSet! Not ArrayList!

--- src/me/choco/veinminer/utils/NonNullHashSet.java
+++ src/me/choco/veinminer/utils/NonNullHashSet.java
@@ -18,14 +18,14 @@ public class NonNullHashSet<T> extends HashSet<T> {
 	
 	@Override
 	public boolean add(T element) {
-		Preconditions.checkNotNull(element, "NonNullArrayList does not support the addition of null values");
+		Preconditions.checkNotNull(element, "NonNullHashSet does not support the addition of null values");
 		return super.add(element);
 	}
 	
 	@Override
 	public boolean addAll(Collection<? extends T> c) {
-		Preconditions.checkNotNull(c, "NonNullArrayList does not support the addition of null values");
-		Preconditions.checkState(!c.contains(null), "NonNullArrayList does not support the addition of null values");
+		Preconditions.checkNotNull(c, "NonNullHashSet does not support the addition of null values");
+		Preconditions.checkState(!c.contains(null), "NonNullHashSet does not support the addition of null values");
 		
 		return super.addAll(c);
 	}
