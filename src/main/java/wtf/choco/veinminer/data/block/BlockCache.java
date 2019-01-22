6edfb94062dcce8dcda5faf1cf99625a93b54f37
Parker Hawke <hawkeboyz2@hotmail.com>
2019-01-22 16:26:52 -0500

    Remove unnecessary Supplier in BlockCache

--- src/main/java/wtf/choco/veinminer/data/block/BlockCache.java
+++ src/main/java/wtf/choco/veinminer/data/block/BlockCache.java
@@ -4,20 +4,19 @@ import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 
 final class BlockCache<T> {
 
-	protected static final BlockCache<Material> MATERIAL = new BlockCache<>(() -> new EnumMap<>(Material.class));
-	protected static final BlockCache<BlockData> BLOCK_DATA = new BlockCache<>(() -> new HashMap<>()); // No constructor reference due to type inference "bug"
+	protected static final BlockCache<Material> MATERIAL = new BlockCache<>(new EnumMap<>(Material.class));
+	protected static final BlockCache<BlockData> BLOCK_DATA = new BlockCache<>(new HashMap<>());
 
 	private final Map<T, VeinBlock> cached;
 
-	private BlockCache(Supplier<Map<T, VeinBlock>> mapSupplier) {
-		this.cached = mapSupplier.get();
+	private BlockCache(Map<T, VeinBlock> backingMap) {
+		this.cached = backingMap;
 	}
 
 	protected VeinBlock getOrCache(T type, Function<T, VeinBlock> defaultSupplier) {
