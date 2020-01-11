76e6c6f46aff609279f82414876295c1df71cfd2
Parker Hawke <hawkeboyz2@hotmail.com>
2020-01-11 10:32:49 -0500

    Fix the expansive pattern mining one more than the max vein size

--- src/main/java/wtf/choco/veinminer/pattern/PatternExpansive.java
+++ src/main/java/wtf/choco/veinminer/pattern/PatternExpansive.java
@@ -55,7 +55,7 @@ public final class PatternExpansive implements VeinMiningPattern {
                         continue;
                     }
 
-                    if (blocks.size() + buffer.size() > maxVeinSize) {
+                    if (blocks.size() + buffer.size() >= maxVeinSize) {
                         break recentSearch;
                     }
 
