9fa21b1fbff959bcea4fd877f7a45b153d7e5eb0
Parker Hawke <hawkeboyz2@hotmail.com>
2020-12-14 23:05:24 -0500

    Minor whitespace & formatting adjustments from recent merge

--- src/main/java/wtf/choco/veinminer/anticheat/AntiCheatHookAAC.java
+++ src/main/java/wtf/choco/veinminer/anticheat/AntiCheatHookAAC.java
@@ -1,18 +1,20 @@
 package wtf.choco.veinminer.anticheat;
 
+import java.util.Objects;
+
 import me.konsolas.aac.api.AACAPI;
 import me.konsolas.aac.api.AACExemption;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * The default Advanced AntiCheat (AAC) hook implementation
  */
 public final class AntiCheatHookAAC implements AntiCheatHook {
+
     private final AACExemption exemption = new AACExemption("The player is using VeinMiner");
     private final AACAPI api = Objects.requireNonNull(Bukkit.getServicesManager().load(AACAPI.class));
 
@@ -23,11 +25,12 @@ public final class AntiCheatHookAAC implements AntiCheatHook {
 
     @Override
     public void exempt(@NotNull Player player) {
-        api.addExemption(player, exemption);
+        this.api.addExemption(player, exemption);
     }
 
     @Override
     public void unexempt(@NotNull Player player) {
-        api.removeExemption(player, exemption);
+        this.api.removeExemption(player, exemption);
     }
+
 }
