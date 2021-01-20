ceede30571facd01777e7b64430980dbba3f8a21
Parker Hawke <hawkeboyz2@hotmail.com>
2021-01-20 15:07:44 -0500

    Render an icon when holding the vein mine activation button

--- VeinMiner-Fabric/src/main/java/wtf/choco/veinminer/fabric/VeinMiner.java
+++ VeinMiner-Fabric/src/main/java/wtf/choco/veinminer/fabric/VeinMiner.java
@@ -1,18 +1,27 @@
 package wtf.choco.veinminer.fabric;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.Window;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
 
 import org.lwjgl.glfw.GLFW;
 
 public class VeinMiner implements ClientModInitializer {
+
+    private static final Identifier TEXTURE_VEINMINER_ICON = new Identifier("veinminer4bukkit", "textures/gui/veinminer_icon.png");
 
     private boolean veinminerActivated = false;
 
@@ -46,6 +55,28 @@ public class VeinMiner implements ClientModInitializer {
 
         ClientPlayNetworking.registerGlobalReceiver(VeinMinerBukkitProtocol.CHANNEL_IDENTIFIER, (client, handler, buf, responseSender) -> {
             // For when messages are sent to the client... not yet though.
+        });
+
+        HudRenderCallback.EVENT.register((stack, delta) -> {
+            if (!veinminerActivated) {
+                return;
+            }
+
+            MinecraftClient client = MinecraftClient.getInstance();
+            Window window = client.getWindow();
+            int width = window.getScaledWidth(), height = window.getScaledHeight();
+
+            RenderSystem.pushMatrix();
+            RenderSystem.disableLighting();
+            RenderSystem.disableDepthTest();
+
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            client.getTextureManager().bindTexture(TEXTURE_VEINMINER_ICON);
+            DrawableHelper.drawTexture(stack, (width / 2) + 8, (height / 2) - 4, 0, 0, 8, 8, 8, 8);
+
+            RenderSystem.enableDepthTest();
+            RenderSystem.enableLighting();
+            RenderSystem.popMatrix();
         });
     }
 
