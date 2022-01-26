e565c51078c3c5a4931d7565383d106c1d998504
Parker Hawke <hawkeboyz2@hotmail.com>
2022-01-25 21:23:35 -0500

    Standardize and simplify commands

--- VeinMiner-Bukkit/src/main/java/wtf/choco/veinminer/command/CommandVeinMiner.java
+++ VeinMiner-Bukkit/src/main/java/wtf/choco/veinminer/command/CommandVeinMiner.java
@@ -5,8 +5,10 @@ import com.google.common.base.Optional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
@@ -44,11 +46,9 @@ public final class CommandVeinMiner implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String @NotNull [] args) {
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Invalid command syntax! " + ChatColor.GRAY + "Missing parameter. " + ChatColor.YELLOW + "/" + label + " <version|reload|blocklist|toollist|toggle|pattern|mode>");
-            return true;
+            return false;
         }
 
-        // Reload subcommand
         if (args[0].equalsIgnoreCase("reload")) {
             if (!sender.hasPermission(VMConstants.PERMISSION_COMMAND_RELOAD)) {
                 sender.sendMessage(ChatColor.RED + "You have insufficient permissions to execute this command.");
@@ -63,9 +63,9 @@ public final class CommandVeinMiner implements TabExecutor {
             this.plugin.reloadToolCategoryRegistryConfig();
 
             sender.sendMessage(ChatColor.GREEN + "VeinMiner configuration successfully reloaded.");
+            return true;
         }
 
-        // Version subcommand
         else if (args[0].equalsIgnoreCase("version")) {
             PluginDescriptionFile description = plugin.getDescription();
             String headerFooter = ChatColor.GOLD.toString() + ChatColor.BOLD + ChatColor.STRIKETHROUGH + StringUtils.repeat("-", 44);
@@ -75,77 +75,74 @@ public final class CommandVeinMiner implements TabExecutor {
             sender.sendMessage(ChatColor.GOLD + "Version: " + ChatColor.WHITE + description.getVersion() + getUpdateSuffix());
             sender.sendMessage(ChatColor.GOLD + "Developer: " + ChatColor.WHITE + description.getAuthors().get(0));
             sender.sendMessage(ChatColor.GOLD + "Plugin page: " + ChatColor.WHITE + description.getWebsite());
-            sender.sendMessage(ChatColor.GOLD + "Report bugs to: " + ChatColor.WHITE + "https://github.com/2008Choco/VeinMiner/issues");
+            sender.sendMessage(ChatColor.GOLD + "Source code: " + ChatColor.WHITE + "https://github.com/2008Choco/VeinMiner");
             sender.sendMessage("");
             sender.sendMessage(headerFooter);
+            return true;
         }
 
-        // Toggle subcommand
         else if (args[0].equalsIgnoreCase("toggle")) {
             if (!(sender instanceof Player player)) {
-                sender.sendMessage("VeinMiner cannot be toggled from the console.");
+                sender.sendMessage("Vein miner cannot be toggled from the console.");
                 return true;
             }
 
-            if (!canVeinMine(player)) {
-                player.sendMessage(ChatColor.RED + "You may not toggle a feature to which you do not have access.");
-                return true;
-            }
-
-            if (!player.hasPermission(VMConstants.PERMISSION_COMMAND_TOGGLE)) {
+            if (!canVeinMine(player) || !player.hasPermission(VMConstants.PERMISSION_COMMAND_TOGGLE)) {
                 player.sendMessage(ChatColor.RED + "You have insufficient permissions to execute this command.");
                 return true;
             }
 
             VeinMinerPlayer veinMinerPlayer = plugin.getPlayerManager().get(player);
+
             // Toggle a specific tool
             if (args.length >= 2) {
                 VeinMinerToolCategory category = plugin.getToolCategoryRegistry().get(args[1]);
                 if (category == null) {
-                    player.sendMessage(ChatColor.GRAY + "Invalid tool category: " + ChatColor.YELLOW + args[1]);
+                    player.sendMessage(ChatColor.RED + "Invalid tool category " + args[1] + ".");
                     return true;
                 }
 
                 veinMinerPlayer.setVeinMinerEnabled(!veinMinerPlayer.isVeinMinerEnabled(), category);
-                player.sendMessage(ChatColor.GRAY + "VeinMiner successfully toggled "
-                    + (veinMinerPlayer.isVeinMinerDisabled(category) ? ChatColor.RED + "off" : ChatColor.GREEN + "on")
+                player.sendMessage(ChatColor.GRAY + "Vein miner toggled "
+                    + (veinMinerPlayer.isVeinMinerDisabled(category)
+                            ? ChatColor.RED.toString() + ChatColor.BOLD + "OFF"
+                            : ChatColor.GREEN.toString() + ChatColor.BOLD + "ON"
+                    )
                     + ChatColor.GRAY + " for tool " + ChatColor.YELLOW + category.getId().toLowerCase() + ChatColor.GRAY + ".");
             }
 
             // Toggle all tools
             else {
                 veinMinerPlayer.setVeinMinerEnabled(!veinMinerPlayer.isVeinMinerEnabled());
-                player.sendMessage(ChatColor.GRAY + "VeinMiner successfully toggled "
-                    + (veinMinerPlayer.isVeinMinerEnabled() ? ChatColor.GREEN + "on" : ChatColor.RED + "off")
-                    + ChatColor.GRAY + " for " + ChatColor.YELLOW + "all tools");
+                player.sendMessage(ChatColor.GRAY + "Vein miner toggled "
+                    + (veinMinerPlayer.isVeinMinerDisabled()
+                            ? ChatColor.RED.toString() + ChatColor.BOLD + "OFF"
+                            : ChatColor.GREEN.toString() + ChatColor.BOLD + "ON"
+                    )
+                    + ChatColor.GRAY + " for " + ChatColor.YELLOW + "all tools" + ChatColor.GRAY + ".");
             }
+
+            return true;
         }
 
-        // Mode subcommand
         else if (args[0].equalsIgnoreCase("mode")) {
             if (!(sender instanceof Player player)) {
-                sender.sendMessage("VeinMiner cannot change mode from the console.");
+                sender.sendMessage("Vein miner modes cannot be changed from the console.");
                 return true;
             }
 
-            if (!canVeinMine(player)) {
-                player.sendMessage(ChatColor.RED + "You may not toggle a feature to which you do not have access.");
-                return true;
-            }
-
-            if (!player.hasPermission(VMConstants.PERMISSION_COMMAND_MODE)) {
+            if (!canVeinMine(player) || !player.hasPermission(VMConstants.PERMISSION_COMMAND_MODE)) {
                 player.sendMessage(ChatColor.RED + "You have insufficient permissions to execute this command.");
                 return true;
             }
 
             if (args.length < 2) {
-                player.sendMessage(ChatColor.RED + "Invalid command syntax! " + ChatColor.GRAY + "Missing parameter(s). " + ChatColor.YELLOW + "/" + label + " " + args[0] + " <sneak|stand|always|client>");
-                return true;
+                return false;
             }
 
             Optional<ActivationStrategy> strategyOptional = Enums.getIfPresent(ActivationStrategy.class, args[1].toUpperCase());
             if (!strategyOptional.isPresent()) {
-                player.sendMessage(ChatColor.GRAY + "Invalid activation strategy: " + ChatColor.YELLOW + args[1] + ChatColor.GRAY + ".");
+                player.sendMessage(ChatColor.RED + "Invalid mode " + args[1] + ".");
                 return true;
             }
 
@@ -153,7 +150,7 @@ public final class CommandVeinMiner implements TabExecutor {
             VeinMinerPlayer veinMinerPlayer = plugin.getPlayerManager().get(player);
 
             if (strategy == ActivationStrategy.CLIENT && !veinMinerPlayer.isUsingClientMod()) {
-                player.sendMessage(ChatColor.RED + "You do not have VeinMiner installed on your client!");
+                player.sendMessage(ChatColor.RED + "You do not have VeinMiner4Bukkit installed on your client!");
 
                 // Let them know where to install VeinMiner on the client (if it's allowed)
                 if (plugin.getConfig().getBoolean(VMConstants.CONFIG_CLIENT_ALLOW_CLIENT_ACTIVATION, true)) {
@@ -166,21 +163,23 @@ public final class CommandVeinMiner implements TabExecutor {
             }
 
             veinMinerPlayer.setActivationStrategy(strategy);
-            player.sendMessage(ChatColor.GREEN + "Activation mode successfully changed to " + ChatColor.YELLOW + strategy.name().toLowerCase().replace("_", " ") + ChatColor.GREEN + ".");
+            player.sendMessage(ChatColor.GREEN + "Mode successfully changed to " + ChatColor.YELLOW + strategy.name().toLowerCase().replace("_", " ") + ChatColor.GREEN + ".");
+            return true;
         }
 
-        // Blocklist subcommand
         else if (args[0].equalsIgnoreCase("blocklist")) {
             this.commandBlocklist.execute(sender, args[0], Arrays.copyOfRange(args, 1, args.length));
+            return true;
         }
 
         else if (args[0].equalsIgnoreCase("toollist")) {
             this.commandToollist.execute(sender, args[0], Arrays.copyOfRange(args, 1, args.length));
+            return true;
         }
 
         else if (args[0].equalsIgnoreCase("pattern")) {
             if (!(sender instanceof Player player)) {
-                sender.sendMessage(ChatColor.RED + "The console cannot change its vein miner pattern.");
+                sender.sendMessage("Vein miner patterns cannot be changed from the console.");
                 return true;
             }
 
@@ -190,78 +189,60 @@ public final class CommandVeinMiner implements TabExecutor {
             }
 
             if (args.length < 2) {
-                sender.sendMessage(ChatColor.RED + "Invalid command syntax! " + ChatColor.GRAY + "Missing parameter. " + ChatColor.YELLOW + "/" + label + " " + args[0] + " <pattern_id>");
-                return true;
+                return false;
             }
 
             org.bukkit.NamespacedKey patternKey = org.bukkit.NamespacedKey.fromString(args[1], plugin);
             if (patternKey == null) {
-                sender.sendMessage(ChatColor.RED + "Invalid pattern ID! " + ChatColor.GRAY + "Pattern IDs should be formatted as " + ChatColor.YELLOW + "namespace:id" + ChatColor.GRAY + "(i.e. " + ChatColor.YELLOW + "veinminer:expansive" + ChatColor.GRAY + ").");
+                sender.sendMessage(ChatColor.RED + "Invalid pattern key " + args[1] + ".");
                 return true;
             }
 
             VeinMiningPattern pattern = plugin.getPatternRegistry().get(new NamespacedKey(patternKey.getNamespace(), patternKey.getKey()));
             if (pattern == null) {
-                sender.sendMessage(ChatColor.GRAY + "A pattern with the ID " + ChatColor.YELLOW + patternKey + ChatColor.GRAY + " could not be found.");
+                sender.sendMessage(ChatColor.RED + "A pattern with the key " + patternKey + " could not be found.");
                 return true;
             }
 
             VeinMinerPlayer veinMinerPlayer = plugin.getPlayerManager().get(player);
             veinMinerPlayer.setVeinMiningPattern(pattern);
-            this.plugin.getConfig().set(VMConstants.CONFIG_DEFAULT_VEIN_MINING_PATTERN, pattern.getKey().toString());
-            this.plugin.saveConfig();
 
-            sender.sendMessage(ChatColor.GREEN + "Pattern successfully set to " + ChatColor.YELLOW + patternKey + ChatColor.GRAY + ".");
-        }
-
-        // Unknown command usage
-        else {
-            sender.sendMessage(ChatColor.RED + "Invalid command syntax! " + ChatColor.GRAY + "Unknown parameter, " + ChatColor.AQUA + args[0] + ChatColor.GRAY + ". " + ChatColor.YELLOW + "/" + label + " <version|reload|blocklist|toollist|toggle|pattern|mode>");
+            sender.sendMessage(ChatColor.GREEN + "Pattern set to " + patternKey + ".");
             return true;
         }
 
-        return true;
+        return false;
     }
 
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String commandLabel, String @NotNull [] args) {
-        List<String> values = new ArrayList<>();
-
         if (args.length == 1) {
-            values.add("version");
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_RELOAD)) {
-                values.add("reload");
-            }
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_BLOCKLIST)) {
-                values.add("blocklist");
-            }
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_TOOLLIST)) {
-                values.add("toollist");
-            }
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_TOGGLE)) {
-                values.add("toggle");
-            }
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_MODE)) {
-                values.add("mode");
-            }
-            if (sender.hasPermission(VMConstants.PERMISSION_COMMAND_PATTERN)) {
-                values.add("pattern");
-            }
+            List<String> suggestions = new ArrayList<>();
 
-            return StringUtil.copyPartialMatches(args[0], values, new ArrayList<>());
+            suggestions.add("version");
+            this.addConditionally(suggestions, "reload", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_RELOAD));
+            this.addConditionally(suggestions, "blocklist", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_BLOCKLIST));
+            this.addConditionally(suggestions, "toollist", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_TOOLLIST));
+            this.addConditionally(suggestions, "toggle", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_TOGGLE));
+            this.addConditionally(suggestions, "mode", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_MODE));
+            this.addConditionally(suggestions, "pattern", () -> sender.hasPermission(VMConstants.PERMISSION_COMMAND_PATTERN));
+
+            return StringUtil.copyPartialMatches(args[0], suggestions, new ArrayList<>());
         }
 
-        if (args.length > 1 && args[0].equalsIgnoreCase("blocklist")) {
+        if (args[0].equalsIgnoreCase("blocklist")) {
             return commandBlocklist.tabComplete(sender, commandLabel, Arrays.copyOfRange(args, 1, args.length));
         }
 
-        else if (args.length > 1 && args[0].equalsIgnoreCase("toollist")) {
+        else if (args[0].equalsIgnoreCase("toollist")) {
             return commandToollist.tabComplete(sender, commandLabel, Arrays.copyOfRange(args, 1, args.length));
         }
 
         else if (args.length == 2) {
+            List<String> suggestions = new ArrayList<>();
+
             if (args[0].equalsIgnoreCase("toggle")) {
-                this.plugin.getToolCategoryRegistry().getAll().forEach(category -> values.add(category.getId().toLowerCase()));
+                this.plugin.getToolCategoryRegistry().getAll().forEach(category -> suggestions.add(category.getId().toLowerCase()));
             }
 
             else if (args[0].equalsIgnoreCase("mode")) {
@@ -270,7 +251,7 @@ public final class CommandVeinMiner implements TabExecutor {
                         continue;
                     }
 
-                    values.add(activationStrategy.name().toLowerCase());
+                    suggestions.add(activationStrategy.name().toLowerCase());
                 }
             }
 
@@ -278,17 +259,21 @@ public final class CommandVeinMiner implements TabExecutor {
                 for (VeinMiningPattern pattern : plugin.getPatternRegistry().getPatterns()) {
                     String patternKey = pattern.getKey().toString();
                     if (patternKey.contains(args[1])) {
-                        values.add(patternKey);
+                        suggestions.add(patternKey);
                     }
                 }
             }
+
+            return StringUtil.copyPartialMatches(args[1], suggestions, new ArrayList<>());
         }
 
-        else {
-            return Collections.emptyList();
-        }
+        return Collections.emptyList();
+    }
 
-        return StringUtil.copyPartialMatches(args[args.length - 1], values, new ArrayList<>());
+    private <T> void addConditionally(Collection<T> collection, T value, BooleanSupplier predicate) {
+        if (predicate.getAsBoolean()) {
+            collection.add(value);
+        }
     }
 
     private boolean canVeinMine(Player player) {
