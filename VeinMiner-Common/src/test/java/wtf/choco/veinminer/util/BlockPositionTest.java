eb216e6cbf01ed8e949400da049ece7561ddf562
Parker Hawke <hawkeboyz2@hotmail.com>
2022-01-29 11:11:28 -0500

    Additional test cases for BlockPosition

--- VeinMiner-Common/src/test/java/wtf/choco/veinminer/util/BlockPositionTest.java
+++ VeinMiner-Common/src/test/java/wtf/choco/veinminer/util/BlockPositionTest.java
@@ -1,5 +1,7 @@
 package wtf.choco.veinminer.util;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -7,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 class BlockPositionTest {
+
+    private static final int MAX_XZ = 30000000, MAX_Y = 320, MIN_Y = -64;
 
     @Test
     void testOffset() {
@@ -33,6 +37,23 @@ class BlockPositionTest {
 
         // Double check that a zero'd BlockPosition and the zero obtained from an offset are equals()
         assertEquals(new BlockPosition(0, 0, 0), offsetNegative);
+    }
+
+    @Test
+    void testPack() {
+        BlockPosition position = new BlockPosition(0, 0, 0);
+        assertEquals(0, position.pack());
+    }
+
+    @Test
+    void testPackUnpack() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int x = random.nextInt(-MAX_XZ, MAX_XZ), y = random.nextInt(MIN_Y, MAX_Y), z = random.nextInt(-MAX_XZ, MAX_XZ);
+
+        BlockPosition position = new BlockPosition(x, y, z);
+        BlockPosition unpacked = BlockPosition.unpack(position.pack());
+
+        assertEquals(position, unpacked);
     }
 
 }
