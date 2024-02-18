12121e1415fa235c2605b8f277f0d21144693381
2008Choco <hawkeboyz2@hotmail.com>
2024-02-17 22:43:27 -0500

    Update Networking to 0.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ subprojects {
     repositories {
         mavenCentral()
         maven { url = uri("http://repo.choco.wtf/releases"); isAllowInsecureProtocol = true }
+        maven { url = uri("http://repo.choco.wtf/snapshots"); isAllowInsecureProtocol = true }
     }
 
     dependencies {
